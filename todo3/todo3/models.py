c47c409085f2efede23b99c01b65d06411f08fb3
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2021-10-07 22:05:01 +0300

    update model, index

--- todo3/todo3/models.py
+++ todo3/todo3/models.py
@@ -7,19 +7,25 @@ class Todolist(models.Model):
     todoitem_done = models.BooleanField()
     
     def item_class(self):
-
         classes = []
-
         if self.todoitem_fav :
             classes.append("list-group-item-success")
-
         if self.todoitem_done:
             classes.append("list-group-item-strikethrough")
-
         if not classes:
             classes.append("list-group-item-light") 
-        
         return " ".join(classes)
+    
+
+    def btn_done_class(self):
+        classes = []
+        if self.todoitem_done:
+            classes.append("btn-success") 
+        else:
+            classes.append("btn-outline-success")
+        return " ".join(classes)
+
+
 
     def __str__(self):
         # return f"Todolist ID>>> {self.id}, Todoitem>>> {self.todoitem}"

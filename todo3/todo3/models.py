0f5c3b3aba4de370f56e6f8a45812b678d0ac9dd
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-01-10 11:34:40 +0200

    Fixed cat selected error

--- todo3/todo3/models.py
+++ todo3/todo3/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TodoCat(models.Model):
     name = models.CharField(max_length=400, unique=True, blank=False)
-    selected = models.BooleanField()
+    selected = models.BooleanField(default=0)
 
     def item_class(self):
         classes = []
